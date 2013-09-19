dc206d07759b78dd0176a39eb65c0c3e7ebdc2c4
Jeremy Dye <dyeje@mail.gvsu.edu>
2013-09-19 17:08:42 -0400

    Meteor out.

--- src/worphles/server/js/Dictionary.js
+++ src/worphles/server/js/Dictionary.js
@@ -1,13 +1,10 @@
-var fileContent;
+var fs = Npm.require('fs');
 
-Dictionary = function() {
-	var fs = Npm.require('fs');
+var text = fs.readFileSync('localhost:3000/words.txt', "utf-8"); //replace this with relative path
+var wordList = text.split("\n");
 
-	var text = fs.readFileSync('localhost:3000/words.txt', "utf-8"); //replace this with relative path
-	var wordList = text.split("\n");
+var dictionary = {};
 
-	var dictionary = {};
+for(var i = 0; i < wordList.length; i++)
+  dictionary[wordList[i]] = true;
 
-	for(var i = 0; i < wordList.length; i++)
-		dictionary[wordList[i]] = true;
-}
