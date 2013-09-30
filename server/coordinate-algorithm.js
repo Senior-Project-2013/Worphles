869e3f6ed23db45447b1fd15b962124043627a17
jordonbiondo <biondoj@mail.gvsu.edu>
2013-09-30 16:10:28 -0400

    prototypish coordinate system

--- server/coordinate-algorithm.js
+++ server/coordinate-algorithm.js
@@ -1,24 +1,42 @@
 var gm = require('./geometry.js');
+var assert = require('assert');
 
-function nextSpace(side, point, direction) {
-  var dirV = directionToVector(direction);
-  return point.add(dirV);
+function tileToIndex(tile, size) {
+  return (tile.side * (size * size)) + (tile.pos.y * size) + tile.pos.x;
+}
+
+
+var DIRECTIONS = {
+  right: 0,
+  upRight: 1,
+  up: 2,
+  upLeft: 3,
+  left: 4,
+  downLeft: 5,
+  down: 6,
+  downRight: 7
 }
 
 function directionToVector(n) {
   switch(n) {
-  case 0: return new gm.Point(1, 0);
-  case 1: return new gm.Point(1, 1);
-  case 2: return new gm.Point(0, 1);
-  case 3: return new gm.Point(-1, 1);
-  case 4: return new gm.Point(-1, 0);
-  case 5: return new gm.Point(-1, -1);
-  case 6: return new gm.Point(0, -1);
-  case 7: return new gm.Point(1, -1);
+  case DIRECTIONS.right: return new gm.Point(1, 0);
+  case DIRECTIONS.upRight: return new gm.Point(1, 1);
+  case DIRECTIONS.up: return new gm.Point(0, 1);
+  case DIRECTIONS.upLeft: return new gm.Point(-1, 1);
+  case DIRECTIONS.left: return new gm.Point(-1, 0);
+  case DIRECTIONS.downLeft: return new gm.Point(-1, -1);
+  case DIRECTIONS.down: return new gm.Point(0, -1);
+  case DIRECTIONS.downRight: return new gm.Point(1, -1);
   default: return null;;
   }  
 }
 
+var Tile = function(side, point) {
+  return {
+    side: side,
+    pos: point
+  };
+}
 var Side = function() {
   return {
     left: null,
@@ -37,53 +55,39 @@ var SideRelation = function(side, rotations) {
 
 
 var Sides = function() {
-  var side0 = new Side();
-  var side1 = new Side();
-  var side2 = new Side();
-  var side3 = new Side();
-  var side4 = new Side();
-  var side5 = new Side();
-
-  
-  side0.left = new SideRelation(side3, 0);
-  side0.right = new SideRelation(side1, 0);
-  side0.top = new SideRelation(side5, 0);
-  side0.bottom = new SideRelation(side4, 0);
-
-  side1.left = new SideRelation(side0, 0);
-  side1.right = new SideRelation(side2, 0);
-  side1.top = new SideRelation(side5, 1);
-  side1.bottom = new SideRelation(side4, 3);
-  
-  side2.left = new SideRelation(side1, 0);
-  side2.right = new SideRelation(side3, 0);
-  side2.top = new SideRelation(side5, 2);
-  side2.bottom = new SideRelation(side4, 2);
-  
-  side3.left = new SideRelation(side2, 0);
-  side3.right = new SideRelation(side0, 0);
-  side3.top = new SideRelation(side5, 3);
-  side3.bottom = new SideRelation(side4, 1);
-  
-  side4.left = new SideRelation(side3, 3);
-  side4.right = new SideRelation(side1, 1);
-  side4.top = new SideRelation(side0, 0);
-  side4.bottom = new SideRelation(side2, 2);
-  
-  side5.left = new SideRelation(side3, 1);
-  side5.right = new SideRelation(side1, 3);
-  side5.top = new SideRelation(side2, 2);
-  side5.bottom = new SideRelation(side0, 0);
-
-  return {
-    side0: side0,
-    side1: side1,
-    side2: side2,
-    side3: side3,
-    side4: side4,
-    side5: side5
-  };
+  return [{
+    left: new SideRelation(3, 0),
+    right: new SideRelation(1, 0),
+    top: new SideRelation(5, 0),
+    bottom: new SideRelation(4, 0)
+  },{
+    left: new SideRelation(0, 0),
+    right: new SideRelation(2, 0),
+    top: new SideRelation(5, 1),
+    bottom: new SideRelation(4, 3)
+  },{
+    left: new SideRelation(1, 0),
+    right: new SideRelation(3, 0),
+    top: new SideRelation(5, 2),
+    bottom: new SideRelation(4, 2)
+  },{
+    left: new SideRelation(2, 0),
+    right: new SideRelation(0, 0),
+    top: new SideRelation(5, 3),
+    bottom: new SideRelation(4, 1)
+  },{
+    left: new SideRelation(3, 3),
+    right: new SideRelation(1, 1),
+    top: new SideRelation(0, 0),
+    bottom: new SideRelation(2, 2)
+  },{
+    left: new SideRelation(3, 1),
+    right: new SideRelation(1, 3),
+    top: new SideRelation(2, 2),
+    bottom: new SideRelation(0, 0)
+  }];
 };
+
 
 
 /**
@@ -102,12 +106,65 @@ function applyTransform(point, rotations, n) {
 
 
 
-console.log(nextSpace(null, new gm.Point(0, 0), 0));
-console.log(directionToVector(4).toString());
-console.log(applyTransform(new gm.Point(0, 0), 1, 4));
-console.log(applyTransform(new gm.Point(0, 1), 3, 4));
-console.log(applyTransform(new gm.Point(1, 3), 2, 5));
-console.log(applyTransform(new gm.Point(1, 3), 3, 5));
-console.log(applyTransform(new gm.Point(1, 3), 4, 5));
-console.log(applyTransform(new gm.Point(1, 3), 5, 5));
 
+// console.log(directionToVector(4).toString());
+// console.log(applyTransform(new gm.Point(0, 0), 1, 4));
+// console.log(applyTransform(new gm.Point(0, 1), 3, 4));
+// console.log(applyTransform(new gm.Point(1, 3), 2, 5));
+// console.log(applyTransform(new gm.Point(1, 3), 3, 5));
+// console.log(applyTransform(new gm.Point(1, 3), 4, 5));
+// console.log(applyTransform(new gm.Point(1, 3), 5, 5));
+
+var CubeGrid = function(size) {
+  this.grid = [];
+  this.size = size;
+  for (var i = 0; i < size * size * 6; i++) {
+    this.grid[i] = {
+      right: null,
+      topRight: null,
+      top: null,
+      topLeft: null,
+      left: null,
+      bottomLeft: null,
+      bottom: null,
+      bottomRight: null
+    };
+  }
+  return grid;
+}
+
+
+function indexToTile(index, size) {
+  assert(index > 0 &&index < size*size*6);
+  var side = Math.floor(index / (size * size));
+  var sideIndex = index - side * (size * size);
+  return Tile(side, new gm.Point(sideIndex % size, Math.floor(sideIndex / size)));
+}
+
+function nextTile(tile, direction, size) {
+  console.log(tile);
+  console.log(direction);
+  var sides = new Sides();
+  var nextTile = tile;
+  var thisSide = sides[nextTile.side];
+  nextTile.pos = nextTile.pos.add(directionToVector(direction));
+  if (nextTile.pos.y == size) {
+    nextTile.pos.y = 0;
+    nextTile.side = thisSide.top.side;
+    nextTile.pos = applyTransform(nextTile.pos, thisSide.top.rotations, size);
+  } else if (nextTile.pos.y == -1) {
+    nextTile.pos.y = size -1;
+    nextTile.side = thisSide.botom.side;
+    nextTile.pos = applyTransform(nextTile.pos, thisSide.bottome.rotations, size);
+  } else if (nextTile.pos.x == size) {
+    nextTile.pos.x = 0;
+    nextTile.side = thisSide.right.side;
+    nextTile.pos = applyTransform(nextTile.pos, thisSide.right.rotations, size);
+  } else if (nextTile.pos.x == -1) {
+    nextTile.pos.x = size -1;
+    nextTile.side = thisSide.left.side;
+    nextTile.pos = applyTransform(nextTile.pos, thisSide.left.rotations, size);
+  }
+  console.log(nextTile);
+  return nextTile;
+}
