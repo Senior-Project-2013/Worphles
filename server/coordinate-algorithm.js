60805473e1f70b68b85081d79c06959ee7a326f7
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-01 16:29:00 -0400

    tile relations

--- server/coordinate-algorithm.js
+++ server/coordinate-algorithm.js
@@ -1,35 +1,17 @@
 var gm = require('./geometry.js');
 var assert = require('assert');
 
-function tileToIndex(tile, size) {
-  return (tile.side * (size * size)) + (tile.pos.y * size) + tile.pos.x;
-}
-
 
 var DIRECTIONS = {
-  right: 0,
-  upRight: 1,
-  up: 2,
-  upLeft: 3,
-  left: 4,
-  downLeft: 5,
-  down: 6,
-  downRight: 7
-}
-
-function directionToVector(n) {
-  switch(n) {
-  case DIRECTIONS.right: return new gm.Point(1, 0);
-  case DIRECTIONS.upRight: return new gm.Point(1, 1);
-  case DIRECTIONS.up: return new gm.Point(0, 1);
-  case DIRECTIONS.upLeft: return new gm.Point(-1, 1);
-  case DIRECTIONS.left: return new gm.Point(-1, 0);
-  case DIRECTIONS.downLeft: return new gm.Point(-1, -1);
-  case DIRECTIONS.down: return new gm.Point(0, -1);
-  case DIRECTIONS.downRight: return new gm.Point(1, -1);
-  default: return null;;
-  }  
-}
+  right:  new gm.Point(1, 0),
+  upRight:  new gm.Point(1, 1),
+  up:  new gm.Point(0, 1),
+  upLeft:  new gm.Point(-1, 1),
+  left:  new gm.Point(-1, 0),
+  downLeft:  new gm.Point(-1, -1),
+  down:  new gm.Point(0, -1),
+  downRight:  new gm.Point(1, -1)
+};
 
 var Tile = function(side, point) {
   return {
@@ -37,14 +19,6 @@ var Tile = function(side, point) {
     pos: point
   };
 }
-var Side = function() {
-  return {
-    left: null,
-    right: null,
-    abov: null,
-    below: null
-  };
-};
 
 var SideRelation = function(side, rotations) {
   return {
@@ -53,9 +27,11 @@ var SideRelation = function(side, rotations) {
   };
 };
 
-
-var Sides = function() {
-  return [{
+/**
+ * Sides
+ */
+var Sides =  [
+  {
     left: new SideRelation(3, 0),
     right: new SideRelation(1, 0),
     top: new SideRelation(5, 0),
@@ -86,8 +62,6 @@ var Sides = function() {
     top: new SideRelation(2, 2),
     bottom: new SideRelation(0, 0)
   }];
-};
-
 
 
 /**
@@ -104,67 +78,77 @@ function applyTransform(point, rotations, n) {
   return point;
 }
 
-
-
-
-// console.log(directionToVector(4).toString());
-// console.log(applyTransform(new gm.Point(0, 0), 1, 4));
-// console.log(applyTransform(new gm.Point(0, 1), 3, 4));
-// console.log(applyTransform(new gm.Point(1, 3), 2, 5));
-// console.log(applyTransform(new gm.Point(1, 3), 3, 5));
-// console.log(applyTransform(new gm.Point(1, 3), 4, 5));
-// console.log(applyTransform(new gm.Point(1, 3), 5, 5));
-
+/**
+ * The game board
+ */
 var CubeGrid = function(size) {
-  this.grid = [];
-  this.size = size;
+
+  var grid = [];
   for (var i = 0; i < size * size * 6; i++) {
-    this.grid[i] = {
-      right: null,
-      topRight: null,
-      top: null,
-      topLeft: null,
-      left: null,
-      bottomLeft: null,
-      bottom: null,
-      bottomRight: null
-    };
+    grid[i] = indexToTile(i, size);
   }
-  return grid;
-}
+
+  return {
+    grid: grid,
+    size: size,
+    sides: Sides,
+    getTile: function(index) {
+      return this.grid[index];
+    },
+    nextTile: function(index, direction) {
+      var thisTile = this.getTile(index);
+      var corner = this.tileCorner(index);
+      
+      if (corner != null && direction == corner) return null;
+
+      var nextTile = Tile(thisTile.side, thisTile.pos);
+      var thisSide = this.sides[nextTile.side];
+      nextTile.pos = nextTile.pos.add(direction);
+      if (nextTile.pos.y == size) {
+	nextTile.pos.y = 0;
+	nextTile.side = thisSide.top.side;
+	nextTile.pos = applyTransform(nextTile.pos, thisSide.top.rotations, size);
+      } else if (nextTile.pos.y == -1) {
+	nextTile.pos.y = size -1;
+	nextTile.side = thisSide.botom.side;
+	nextTile.pos = applyTransform(nextTile.pos, thisSide.bottome.rotations, size);
+      } else if (nextTile.pos.x == size) {
+	nextTile.pos.x = 0;
+	nextTile.side = thisSide.right.side;
+	nextTile.pos = applyTransform(nextTile.pos, thisSide.right.rotations, size);
+      } else if (nextTile.pos.x == -1) {
+	nextTile.pos.x = size -1;
+	nextTile.side = thisSide.left.side;
+	nextTile.pos = applyTransform(nextTile.pos, thisSide.left.rotations, size);
+      }
+      return nextTile;
+    },
+    
+    tileIndex: function(tile) {
+      return (tile.side * (size * size)) + (tile.pos.y * size) + tile.pos.x;
+    },
+    
+    tileCorner: function(index) {
+      var sizeSqr = this.size*this.size;
+      var t = index - (sizeSqr * Math.floor(index / sizeSqr));
+      if (t == 0) return DIRECTIONS.downLeft;
+      else if (t == this.size-1) return DIRECTIONS.downRight;
+      else if (t == sizeSqr - this.size) return DIRECTIONS.upLeft;
+      else if (t == sizeSqr - 1) return DIRECTIONS.upRight;
+      else return null;
+    }
+  };
+};
 
 
+
+
+/**
+ * Initialize a Tile based on an index on a sized CubeGrid 
+ */
 function indexToTile(index, size) {
-  assert(index > 0 &&index < size*size*6);
+  assert(index >=0 && index < size*size*6);
   var side = Math.floor(index / (size * size));
   var sideIndex = index - side * (size * size);
   return Tile(side, new gm.Point(sideIndex % size, Math.floor(sideIndex / size)));
-}
-
-function nextTile(tile, direction, size) {
-  console.log(tile);
-  console.log(direction);
-  var sides = new Sides();
-  var nextTile = tile;
-  var thisSide = sides[nextTile.side];
-  nextTile.pos = nextTile.pos.add(directionToVector(direction));
-  if (nextTile.pos.y == size) {
-    nextTile.pos.y = 0;
-    nextTile.side = thisSide.top.side;
-    nextTile.pos = applyTransform(nextTile.pos, thisSide.top.rotations, size);
-  } else if (nextTile.pos.y == -1) {
-    nextTile.pos.y = size -1;
-    nextTile.side = thisSide.botom.side;
-    nextTile.pos = applyTransform(nextTile.pos, thisSide.bottome.rotations, size);
-  } else if (nextTile.pos.x == size) {
-    nextTile.pos.x = 0;
-    nextTile.side = thisSide.right.side;
-    nextTile.pos = applyTransform(nextTile.pos, thisSide.right.rotations, size);
-  } else if (nextTile.pos.x == -1) {
-    nextTile.pos.x = size -1;
-    nextTile.side = thisSide.left.side;
-    nextTile.pos = applyTransform(nextTile.pos, thisSide.left.rotations, size);
-  }
-  console.log(nextTile);
-  return nextTile;
 }
