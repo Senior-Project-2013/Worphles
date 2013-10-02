d78c91b2e9421a535d09e83e23cc33f1a5f7965e
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-02 12:59:46 -0400

    use cube grid validation

--- server/path_validator.js
+++ server/path_validator.js
@@ -1,3 +1,25 @@
-exports.isAPath = function (path) {
-  return true;
-}
+var relations = require('./cube_grid.js');
+
+/**
+ * Determine if a list of indices make a valid path
+ */
+var isAPath = function (path, size) {
+  var cube = new relations.CubeGrid(size);
+  return cube.isValidPath(path);
+};
+
+
+/**
+ * Exports
+ */
+module.exports = {
+  isAPath: isAPath
+};
+
+
+
+
+
+
+
+
