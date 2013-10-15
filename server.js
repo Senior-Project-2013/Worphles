c8fdd013865494787dce7237c567cf3639938c9e
Caleb Gomer <calebgomer@gmail.com>
2013-10-15 16:45:41 -0400

    removed dead code

--- server.js
+++ server.js
@@ -19,11 +19,6 @@ io.configure(function () {
   }
   io.set('log level', 1);
 });
-// let client know if we're on localhost or not
-// app.get('/environment.js', function(req, res) {
-//   res.setHeader("Content-Type", 'application/javascript');
-//   res.send('var WEBSOCKETS_URL = \'' + (process.env.WEBSOCKETS_URL || 'http://localhost') +'\';');
-// });
 
 // serve all webui contents statically
 app.use('/webui', express.static(__dirname + '/webui'));
