492db5c67049217a2259e09cc003abfab95df89e
Caleb Gomer <calebgomer@gmail.com>
2013-10-24 23:13:32 -0400

    #SHRINKIFICATION

--- client/js/scoreboard.js
+++ client/js/scoreboard.js
@@ -1,27 +1,14 @@
 function Scoreboard() {
-
   this.update = function(players) {
     $('#scores').empty();
-
     this.players = players ? players : {};
-
     for(var i = 0; i < Object.keys(players).length; i++) {
       this.createScoreRow(i, players[Object.keys(players)[i]]);
     }
-
     this.resort();
   }
 
-  /* creates a score row */
   this.createScoreRow = function(num, player) {
-    var color = {
-      r: (player.color.r * 255),
-      g: (player.color.g * 255),
-      b: (player.color.b * 255)
-    }
-    console.log('creating player',num,player);
-    console.log('color',getCSSColorFromColor(player.color));
-
     $('<li/>', {
       id: 'score'+player.id,
       class: 'scoreRow',
@@ -39,7 +26,6 @@ function Scoreboard() {
     }).appendTo('#score'+player.id);
   }
 
-  /* updates the score display */
   this.updateScoreDisplay = function(id, score) {
     console.log('updating score display');
     $("#score" + id + " .scoreValue").text(score);
@@ -47,7 +33,6 @@ function Scoreboard() {
     this.resort();
   }
 
-  /* resorts the score rows by score */
   this.resort = function() {
     return $('#scores > li').tsort({ order: 'desc', attr: 'score' });
   }
