d0baa3d5c9ffb331f24fa71bdbaba19c7466334e
jordonbiondo <biondoj@mail.gvsu.edu>
2013-10-26 12:58:56 -0400

    var style in imports

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var Game = require('./server/game'),
-    express = require('express'),
-    app = express(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    _ = require('underscore'),
-    async = require('async');
+var Game = require('./server/game');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var _ = require('underscore');
+var async = require('async');
 
 // websockets configuration
 io.configure(function () {
