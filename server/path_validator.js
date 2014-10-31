7089fc6fa495336f1bb05ed3bc3baf095accfec4
jordonbiondo <biondoj@mail.gvsu.edu>
2014-10-31 13:59:06 -0400

    optimized path validator

--- server/path_validator.js
+++ server/path_validator.js
@@ -4,8 +4,7 @@ var relations = require('./cube_grid.js');
  * Determine if a list of indices make a valid path
  */
 var isAPath = function (path, size) {
-  var cube = new relations.CubeGrid(size);
-  return cube.isValidPath(path);
+  return relations.getGrid(size).isValidPath(path);
 };
 
 
